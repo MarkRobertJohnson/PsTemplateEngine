bb04b8bdbe630f7caaf8e0f0ca7692d7d3b4b185
Mark Johnson <mjohnson@axian.com>
2018-11-12 12:36:38 -0800

    Initial Commit

---
+++ Functions/Util/Remove-ItemToTrash.ps1
@@ -0,0 +1,26 @@
+﻿function Remove-ItemToTrash {
+    [CmdletBinding()]
+    param(
+    [Parameter(Mandatory)][string]$Path)
+    Add-Type -AssemblyName Microsoft.VisualBasic
+
+    $item = Get-Item -LiteralPath $Path -ErrorAction SilentlyContinue
+    if ($item -eq $null)
+    {
+        Write-Error("'{0}' not found" -f $Path)
+    }
+    else
+    {
+        $fullpath=$item.FullName
+        Write-Verbose ("Moving '{0}' to the Recycle Bin" -f $fullpath)
+        if (Test-Path -LiteralPath $fullpath -PathType Container)
+        {
+            [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteDirectory($fullpath,'OnlyErrorDialogs','SendToRecycleBin')
+        }
+        else
+        {
+           
+            [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile($fullpath,'OnlyErrorDialogs','SendToRecycleBin')
+        }
+    }
+}
---
+++ Tests/Import-TemplateConfiguration.Tests.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module "$PSScriptRoot\..\PsTemplateEngine.psm1" -force
+
+Describe 'Import-TemplateConfiguration' {
+    Context 'When configuration has template expansions' {
+        $workingFile = "$PSScriptRoot\TestData\temp_template_properties.json"
+        $expandedWorkingFile = "$PSScriptRoot\TestData\temp_template_properties.pstemplate_expanded.json"
+        $origFile = "$PSScriptRoot\TestData\template_properties.json"
+        Copy-Item $origFile $workingFile -force
+        Import-TemplateConfiguration -Path $workingFile -DoNotDeleteExpandedConfigFiles 
+        $expandedConfig = Get-Content $expandedWorkingFile -Raw
+        #Write-Host -ForegroundColor Cyan $expandedConfig
+        
+        It 'expands all template tokens' {
+            $expandedConfig | Should Not Match '\[\[.*?\]\]'          
+        }
+        
+        It 'can evaluate powershell method calls' {
+            $conf = $expandedConfig | ConvertFrom-Json
+            $conf.OctopusProjectNameSlug | Should Match 'ComponentProjectTemplate'
+        }
+    }
+}
+
